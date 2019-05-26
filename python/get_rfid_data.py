d4a679dc751b93ee7e668976411c75f3268a1e46
Bennyson Brecht <49658184+bbrecht02@users.noreply.github.com>
2019-05-26 08:28:34 -0300

    Update get_rfid_data.py
    exchange of  ".strip()" function by ".decode()" function using UTF8 format removing bugs of decode

--- python/get_rfid_data.py
+++ python/get_rfid_data.py
@@ -5,4 +5,4 @@ read = serial.Serial("COM7", baudrate=9600, timeout=1)
 
 while 1:
 	read.write(b"...")
-	print(str(read.readline())).strip() 
+	print(str(read.readline())).decode(encoding='UTF-8',errors='strict')
